a85e14fb859fee008817c9bb6e51c5f9cf226e18
Samuel Jara <spjara.aravena@gmail.com>
2022-07-18 19:17:56 -0400

    Aplicación Funcional y librerias de test agregadas

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,3 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const CharactersByHero = ({alter_ego, characters}) => {
+    if (alter_ego === characters) return <></>;
+
+    return <p>{characters}</p>;
+}
+
 export const HeroCard = ({
     id,
     superhero,
@@ -7,14 +15,28 @@ export const HeroCard = ({
     characters
 }) => {
 
-    const heroImgUrl = ``;
+    const heroImgUrl = `/assets/heroes/${id}.jpg`;
 
     return (
-        <div className="col">
+        <div className="col animate__animated animate__fadeIn mt-1">
             <div className="card">
                 <div className="row no-gutters">
                     <div className="col-4">
-                        <img src="" className="card-img" alt={superhero} />
+                        <img src={heroImgUrl} className="card-img" alt={superhero} />
+                    </div>
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{superhero}</h5>
+                            <p className="card-text">{alter_ego}</p>
+                            <CharactersByHero alter_ego={alter_ego} characters={characters} />
+                            <p className="card-text">
+                                <small className="text-muted">{first_appearance}</small>
+                            </p>
+
+                            <Link to={`/hero/${id}`}>
+                                More...
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,11 +1,17 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
 
+    const { user, logout } = useContext(AuthContext);
+
     const navigate = useNavigate();
 
     const onLogout = () => {
+        logout();
+
         navigate('/login', {
             replace: true
         });
@@ -18,7 +24,7 @@ export const Navbar = () => {
                 className="navbar-brand"
                 to="/"
             >
-                Asociaciones
+                Associations 
             </Link>
 
             <div className="navbar-collapse">
@@ -37,13 +43,20 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
+
+                    <NavLink
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`} 
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-primary">
-                        Samuel
+                        {user?.name}
                     </span>
 
                     <button
